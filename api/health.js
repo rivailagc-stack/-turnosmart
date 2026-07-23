984cb40451d0440b4fa3b481362e6f66ff8db875
rivailagc-stack <rivailagc@gmail.com>
2026-07-23 18:15:00 -0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -2,10 +2,10 @@ module.exports = function handler(req, res) {
   res.status(200).json({
     ok: true,
     app: 'TurnoSmart',
-    version: '35.0.0',
+    version: '36.0.0',
     sgman_configured: Boolean(process.env.SGMAN_TOKEN),
     sgman_list_enabled: true,
-    machine_specific_history_enabled: true,
+    machine_tree_analysis_enabled: true,
     precise_history_analysis_enabled: true,
     limited_actions_enabled: true
   });
