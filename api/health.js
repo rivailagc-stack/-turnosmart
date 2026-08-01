9a817e96411a0be1e8e80034e1bcc5fe8d39fc4a
rivailagc-stack <rivailagc@gmail.com>
2026-08-01 14:23:11 -0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,11 +1,9 @@
-module.exports = (req, res) =>
-  res.status(200).json({
-    ok: true,
-    app: 'TurnoSmart',
-    version: '60.0.0',
-    ios_library_button_fix: true,
-    full_button_file_input_overlay: true,
-    pointer_events_enabled: true,
-    same_file_reselection_enabled: true,
-    sgman_configured: Boolean(process.env.SGMAN_TOKEN)
-  });
+module.exports=(req,res)=>res.status(200).json({
+  ok:true,
+  app:'TurnoSmart',
+  version:'61.0.0',
+  native_ios_picker_enabled:true,
+  real_media_buttons_enabled:true,
+  overlay_input_removed:true,
+  sgman_configured:Boolean(process.env.SGMAN_TOKEN)
+});
