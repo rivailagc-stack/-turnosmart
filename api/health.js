45ae0f54c50a5a0e376f8056dd499f203b530496
rivailagc-stack <rivailagc@gmail.com>
2026-08-01 13:03:22 -0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -2,9 +2,9 @@ module.exports = (req, res) =>
   res.status(200).json({
     ok: true,
     app: 'TurnoSmart',
-    version: '56.0.0',
-    camera_photo_enabled: true,
+    version: '57.0.0',
+    persistent_media_selection_enabled: true,
+    media_preview_enabled: true,
     media_library_enabled: true,
-    camera_video_enabled: true,
     sgman_configured: Boolean(process.env.SGMAN_TOKEN)
   });
