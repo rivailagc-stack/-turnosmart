20042e5477050167e9c1ba0a28829391f6071dab
rivailagc-stack <rivailagc@gmail.com>
2026-08-06 20:43:09 -0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,7 +1,7 @@
 module.exports=(req,res)=>res.status(200).json({
   ok:true,
   app:'TurnoSmart',
-  version:'71.0.0',
+  version:'72.0.0',
   normalize_machine_code_fixed:true,
   visual_training_save_fixed:true,
   industrial_knowledge_center_enabled:true,
@@ -10,6 +10,9 @@ module.exports=(req,res)=>res.status(200).json({
   real_shift_priority_enabled:true,
   oee_board_rows_fixed:true,
   horizontal_row_validation_enabled:true,
+  ecopack_theme_enabled:true,
+  supervisor_fusion_priority_enabled:true,
+  manual_priority_confirmation_enabled:true,
   monthly_management_evolution_enabled:true,
   visual_ai_training_enabled:Boolean(process.env.OPENAI_API_KEY),
   visual_training_cloud_enabled:Boolean(process.env.SUPABASE_URL && process.env.SUPABASE_SERVICE_ROLE_KEY),
